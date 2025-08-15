3a9bdecd658e62db1068f9e380e70269249905eb
Evelyn Iglesias <evelyniglesias2004@gmail.com>
2025-08-14 21:41:32 -0300

    :sparkles: feat: Iniciando API Person - Verbos HTTP

---
+++ APIRestSpringBoot/src/main/java/br/com/evelyniglesiass/services/PersonService.java
@@ -0,0 +1,68 @@
+package br.com.evelyniglesiass.services;
+
+import br.com.evelyniglesiass.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
+public class PersonService {
+
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(PersonService.class.getName());
+
+    public List<Person> findAll() {
+        logger.info("Finding All People!");
+
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+    }
+
+    public Person findById(String id) {
+        logger.info("Finding one Person!");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Evelyn");
+        person.setLastName("Iglesias");
+        person.setAddress("São Leopoldo - Rio Grande do Sul - Brasil");
+        person.setGender("Female");
+
+        return person;
+    }
+
+    public Person create(Person person) {
+        logger.info("Creating one Person!");
+
+        return person;
+    }
+
+    public Person update(Person person) {
+        logger.info("Updating one Person!");
+
+        return person;
+    }
+
+    public void delete(String id) {
+        logger.info("Deleting one Person!");
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("First Name " + i);
+        person.setLastName("Last Name " + i);
+        person.setAddress("Some Address in Brasil");
+        person.setGender("Female");
+
+        return person;
+    }
+}
