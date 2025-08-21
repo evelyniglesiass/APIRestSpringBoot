1ee1f166aeb32f8e2beed4817d6617061cc57f46
Evelyn Iglesias <evelyniglesias2004@gmail.com>
2025-08-20 23:14:49 -0300

    :sparkles: feat: Adicionando Migrations com Flyway

---
+++ APIRestSpringBoot/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+    (1,'Brasil','Evelyn','Female','Iglesias'),
+    (2,'Brasil','Laura','Female','Keenan'),
+    (3,'Brasil','Gabriela','Female','Radavelli'),
+    (4,'Brasil','Hetielle','Female','Matos');
